44a65de3f345427c7d511d713d0d085a1a44122e
Xenon <x.vergara2201@gmail.com>
2023-11-03 21:59:40 +0800

    minor changes

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const SERVER_URL = 'http://api.weatherapi.com';
+export const SERVER_URL = 'https://api.weatherapi.com';
 
 export default axios.create({
   baseURL: SERVER_URL,
